5c8c6e158874cbe9c95153f6acaa1dc17d1d59d7
berezgnoj2010-hue <berezgnoj2010@gmail.com>
2025-12-15 15:00:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const buttons = document.querySelectorAll('.product-card button');
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.product-card button');
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    alert('Товар додано до кошика!');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Товар додано до кошика!');
+    });
   });
 });
